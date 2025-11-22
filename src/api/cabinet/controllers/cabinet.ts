7b2ee80a3fcc671b81b1b7a4da6d2a0a01f84021
Cretan Alexandru <alexandru.cretann@gmail.com>
2025-11-22 15:29:48 +0200

    initial commit of strapi

---
+++ src/api/cabinet/controllers/cabinet.ts
@@ -0,0 +1,7 @@
+/**
+ * cabinet controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::cabinet.cabinet');
---
+++ src/api/cabinet/routes/cabinet.ts
@@ -0,0 +1,7 @@
+/**
+ * cabinet router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cabinet.cabinet');
