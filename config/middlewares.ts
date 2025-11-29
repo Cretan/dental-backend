0ac18f42d10fd8c5a5b2fc9321d2c56ccfff765b
Cretan Alexandru <alexandru.cretann@gmail.com>
2025-11-29 22:39:51 +0200

    enable frontend

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,7 +5,13 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:4173', 'http://localhost:5174'],
+      origin: [
+        'http://localhost:5173', 
+        'http://127.0.0.1:5173', 
+        'http://localhost:3000', 
+        'http://localhost:4173', 
+        'http://localhost:5174'
+      ],
       headers: '*',
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       keepHeaderOnError: true,
--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '127.0.0.1'), // Use IPv4 explicitly to avoid IPv6 issues
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
